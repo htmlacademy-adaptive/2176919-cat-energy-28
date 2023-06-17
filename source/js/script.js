e7407d0a235e8900542e09e7162c97086eb01b77
Floias <irina.butcher@gmail.com>
2023-06-17 13:13:59 +0500

    исправления

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,6 @@
 const header = document.querySelector('.main-header__container');
 const navMain = document.querySelector('.main-navigation');
-const navToggle = document.querySelector('.main-navigation__toggle');
+const navToggle = document.querySelector('.main-header__toggle-navigation');
 
 if (header) {
   header.classList.remove('main-header__container--nojs');
